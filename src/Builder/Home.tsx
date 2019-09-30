e50fca1d37487e42cd4a831d7048f8c8d88e86c4
Shotaro Chiba <schiba@techlab.jp>
2019-10-01 04:16:25 +0900

    added animation

--- src/Builder/Home.tsx
+++ src/Builder/Home.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { CSSTransition } from 'react-transition-group'
 import { builder, BuilderComponent } from '@builder.io/react'
 import { NotFound } from './../Errors/NotFound'
+import './Home.css'
 
 builder.init('81aa9277c42f4db9bd37132f8f2b29f8')
 
@@ -9,6 +11,7 @@ interface HomeProps {
 
 interface HomeState {
     notFound: boolean
+    isLoaded: boolean
 }
 
 class Home extends React.Component<HomeProps, HomeState> {
@@ -17,21 +20,31 @@ class Home extends React.Component<HomeProps, HomeState> {
 
         this.state = {
             notFound: false,
+            isLoaded: false,
         }
     }
 
     render() {
         return (
             ! this.state.notFound ? (
-            <BuilderComponent
-                name={ 'page' }
-                entry={ '8eba9528293c412ca905f7c7c522e227' }
-                contentLoaded={ (data) => {
-                    if (data === undefined) {
-                        this.setState({ notFound: true, })
-                    }
-                } }
-            /> ) : (
+                <CSSTransition
+                    in={ this.state.isLoaded }
+                    timeout={ 300 }
+                    classNames={ 'Builder' }
+                >
+                    <BuilderComponent
+                        name={ 'page' }
+                        entry={ '8eba9528293c412ca905f7c7c522e227' }
+                        contentLoaded={ (data) => {
+                            if (data === undefined) {
+                                this.setState({ notFound: true, })
+                            } else {
+                                this.setState({ isLoaded: true, })
+                            }
+                        } }
+                    />
+                </CSSTransition>
+            ) : (
                 <NotFound />
             )
         )
